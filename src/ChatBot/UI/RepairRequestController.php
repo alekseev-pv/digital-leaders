a3dd211977076914fa973b8d3103e55e255a63b2
permanovd <permanovd@gmail.com>
2019-06-09 12:36:38 +0300

    refs #2: Add cors bundle. Add messages response in chat bot.

--- src/ChatBot/UI/RepairRequestController.php
+++ src/ChatBot/UI/RepairRequestController.php
@@ -58,4 +58,53 @@ class RepairRequestController
         return new JsonResponse(['id' => $requestId], Response::HTTP_CREATED);
     }
 
+    public function replyToMessage(Request $request): Response
+    {
+        $content = $request->getContent();
+        $this->logger->debug('Message came to api', [$content]);
+        $payload = $this->decoder->decode($content, 'json');
+        $message = $payload['message'];
+        $messageNumber = (int)$payload['message_number'];
+
+        if (1 === $messageNumber) {
+            $greetingActions = [
+                'Задать вопрос',
+                'Отправить заявку',
+                'Оставить жалобу/предложение',
+            ];
+            $arr = [
+                'text' => 'Привет, я бот службы ЖКХ мастер. Буду рад помочь.',
+                'actions' => $greetingActions,
+            ];
+
+            return new JsonResponse($arr, Response::HTTP_OK);
+        }
+
+        $actions = [
+            'Когда планируют отключить горячую воду?' => 'В понедельник 12.09.2019',
+            'Когда будет кап ремонт?' => 'Планируемая дата капитального ремонта: март 2021 года.',
+        ];
+
+        $isOneOfKnownQuestions = array_key_exists($message, $actions);
+        if ('Задать вопрос' === $message) {
+            $arr = [
+                'text' => 'Вы можете узнать ответы на вопросы:',
+                'actions' => array_keys($actions),
+            ];
+
+        } elseif ($isOneOfKnownQuestions) {
+            $arr = [
+                'text' => $actions[$message],
+                'actions' => ['Хотите задать еще вопрос?'],
+            ];
+        } else {
+            $arr = [
+                'text' => 'Вы можете узнать ответы на вопросы:',
+                'actions' => array_keys($actions),
+            ];
+        }
+
+        return new JsonResponse($arr, Response::HTTP_OK);
+    }
+
 }
